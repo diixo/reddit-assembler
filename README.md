275b5e111dc6b9f970bbd1b769e7b92042d2f9d6
diixo <anatolii.podoba@gmail.com>
2025-08-06 20:00:22 +0300

    class RedditAssembler++

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # reddit-assembler
+
+
+## Reddit:
+
+* https://github.com/ArthurHeitmann/arctic_shift
+* https://academictorrents.com/browse.php?search=Reddit
+
--- main.py
+++ main.py
@@ -4,24 +4,85 @@ import zstandard as zstd
 
 import io
 import json
+import re
+from collections import Counter
+import csv
 
 
-def print_submission(data_obj: dict):
-    title = data_obj["title"]
-    txt = data_obj["selftext"]
-    id = data_obj["id"]
-    subreddit = data_obj["subreddit"]
-    print(title)
-    print(txt)
-    print(id)
-    print("subreddit:", subreddit)
-    print("score:", data_obj["score"])
-    print("num_comments:", data_obj["num_comments"])
-    print("is_text:", data_obj["is_self"])
-    print("18+:", data_obj["over_18"])
+def str_tokenize_words(s: str):
+    s = re.findall("(\.?\w[\w'\.&-]*\w|\w\+*#?)", s)
+    if s: return s
+    return []
 
 
-def test(file_path: str):
+class RedditAssembler:
+
+    def __init__(self):
+
+        self.subreddit_counter = Counter()
+        self.dictionary = Counter()
+
+        self.total_count = 0
+        self.valid_count = 0
+
+
+    def add_submission(self, data_obj: dict):
+        self.total_count += 1
+
+        title = data_obj.get("title")
+        if title:
+            self.dictionary.update(str_tokenize_words(title.replace('’', "'")))
+
+        txt = data_obj.get("selftext")
+        if txt:
+            self.dictionary.update(str_tokenize_words(txt.replace('’', "'")))
+
+        num_comments = data_obj.get("num_comments", 0)
+        if int(num_comments) > 0:
+            self.valid_count += 1
+
+        # id = data_obj["id"]
+        # subreddit = data_obj["subreddit"]
+        # print(title)
+        # print(txt)
+        # print(id)
+        # print("subreddit:", subreddit)
+        # print("score:", data_obj["score"])
+        # print("num_comments:", data_obj["num_comments"])
+        # print("is_text:", data_obj["is_self"])
+        # print("18+:", data_obj["over_18"])
+
+        subreddit = data_obj.get("subreddit")
+        if subreddit:
+            self.subreddit_counter[subreddit] += 1
+
+
+    def save_csv(self):
+
+        sorted_subreddits = self.subreddit_counter.most_common()
+
+        # print-out only top
+        # for subreddit, count in sorted_subreddits[:500]:
+        #     print(f"{subreddit}: {count}")
+
+        with open("train-results/subreddit-top-500.csv", "w", newline='', encoding="utf-8") as f:
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow(["subreddit", "comment_count"])
+            writer.writerows(sorted_subreddits)
+
+        sorted_words = sorted(
+            [word for word, _ in self.dictionary.most_common(250000)]
+            )
+
+        # Запись только слов в текстовый файл
+        with open("train-results/dictionary-top-250000.txt", "w", encoding="utf-8") as f:
+            for word in sorted_words:
+                f.write(word + "\n")
+
+        print(f"submissions: total={self.total_count}, valid={self.valid_count}, invalid={self.total_count-self.valid_count}")
+
+
+def test(file_path: str, assembler: RedditAssembler):
 
     with open(file_path, 'rb') as compressed:
         dctx = zstd.ZstdDecompressor()
@@ -29,23 +90,25 @@ def test(file_path: str):
             text_stream = io.TextIOWrapper(reader, encoding='utf-8')
             for i, line in enumerate(text_stream):
 
-                if i > 0:  # Только первые 10 строк для примера
-                   break
+                # if i > 10:
+                #    break
 
                 try:
                     data = json.loads(line)
-                    #print(data)
-                    #print_submission(data)
+                    assembler.add_submission(data)
                 except json.JSONDecodeError:
                     print(f"Ошибка при разборе строки {i}: {line[:100]}")
 
                 if i % 1000 == 0 and i > 0:
                     print("total_items:", str(i))
-                
 
 
 if __name__ == "__main__":
 
     file_path = "C:/utorrent/2025-04/submissions/RS_2025-04.zst"
 
-    test(file_path)
+    assembler = RedditAssembler()
+
+    test(file_path, assembler)
+
+    assembler.save_csv()
