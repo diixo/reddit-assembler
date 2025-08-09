75eb993cda4169d3c8adbd83fd4e65b800f88a3b
diixo <anatolii.podoba@gmail.com>
2025-08-09 22:21:43 +0300

    main_eli5.py: final refactoring finished

--- main_eli5.py
+++ main_eli5.py
@@ -2,22 +2,68 @@
 # https://huggingface.co/datasets/sentence-transformers/eli5
 
 import pyarrow.parquet as pq
+import json
 import pandas as pd
+from collections import Counter
+from pathlib import Path
+from utils import read_embedded_dict, filter_dictionary, clean_text, str_tokenize_words
 
 
-file_path = 'datasets/eli5/pair/train-00000-of-00001.parquet'
+path = Path("data/eli5-dictionary-counter.json")
 
 
-if __name__ == "__main__":
+def processing(dictionary: Counter):
 
-    chunk_df = pd.read_parquet(file_path, columns=["question", "answer"])
+    chunk_df = pd.read_parquet("datasets/eli5/pair/train-00000-of-00001.parquet", columns=["question", "answer"])
 
     first_20 = chunk_df.head(20)
 
     #print(first_20)
 
     for idx, row in chunk_df.iterrows():
+
         question = row["question"]
         answer = row["answer"]
-        print(f"{idx}: Q: {question}\n      A: {answer}\n")
-        break
+        #print(f"{idx}::question: {question}\nanswer: {answer}")
+
+        if (idx + 1) % 1000 == 0:
+            print(f"...items={idx}")
+
+        txt = clean_text(question + " " + answer)
+
+        dictionary.update(str_tokenize_words(txt))
+
+    print(f"total items={idx}")
+
+
+if __name__ == "__main__":
+
+
+    dictionary = Counter()
+
+    if path.exists():
+        with path.open("r", encoding="utf-8") as f:
+            dictionary = Counter(json.load(f))
+
+
+    skip_words = read_embedded_dict()
+
+    processing(dictionary)
+
+    ############# save results
+
+    if len(dictionary.items()) > 0:
+        dictionary = Counter(filter_dictionary(dictionary, skip_words))
+        print("Saved json.sz=", len(dictionary.items()))
+
+        with path.open("w", encoding="utf-8") as f:
+            json.dump(dictionary, f, indent=2)
+
+    most_common = dictionary.most_common()
+
+    sorted_words = [word for word, _ in most_common]
+
+
+    with open(f"data/eli5e-dictionary.txt", "w", encoding="utf-8") as f:
+        for word in sorted_words:
+            f.write(word + "\n")
