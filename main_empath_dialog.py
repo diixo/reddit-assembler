139c41d4da5e4e0fa848a82915a8ba728bf73242
diixo <anatolii.podoba@gmail.com>
2025-08-09 06:53:47 +0300

    main_empath_dialog.py: updated + optimization

--- main_empath_dialog.py
+++ main_empath_dialog.py
@@ -1,17 +1,15 @@
 
-import re
+import json
 from datasets import load_dataset
 from collections import Counter
 from main_owt2 import read_embedded_dict, filter_dictionary, clean_text, str_tokenize_words
+from pathlib import Path
 
 
+path = Path("data/empath-dictionary-counter.json")
 
-if __name__ == "__main__":
 
-    skip_words = read_embedded_dict()
-
-    dictionary = Counter()
-
+def processing(dictionary: Counter):
     dataset = load_dataset("empathetic_dialogues", trust_remote_code=True)
 
     cntr = 0
@@ -25,23 +23,40 @@ if __name__ == "__main__":
             txt = item["utterance"]
             txt = clean_text(txt.replace("_", " ").replace(":", " ").replace(",", " "))
 
-            # tokens = str_tokenize_words(txt)
-            # if "17051" in set(tokens):
-            #     original = item["utterance"]
-            #     print(original)
-            #     print(64*"*"+"\n", txt)
-
             dictionary.update(str_tokenize_words(txt))
-            dictionary = Counter(filter_dictionary(dictionary, skip_words))
+    # total items = 99646
+    print(f"total items={cntr}")
 
-            most_common = dictionary.most_common()
 
-            sorted_words = [word for word, _ in most_common]
+if __name__ == "__main__":
+
+    dictionary = Counter()
+
+    if path.exists():
+        with path.open("r", encoding="utf-8") as f:
+            dictionary = Counter(json.load(f))
+
+
+    skip_words = read_embedded_dict()
+
+    #processing(dictionary)
+
+    ############# save results
+
+    if len(dictionary.items()) > 0:
+        dictionary = Counter(filter_dictionary(dictionary, skip_words))
+        print("Saved json.sz=", len(dictionary.items()))
+
+        with path.open("w", encoding="utf-8") as f:
+            json.dump(dictionary, f, indent=2)
+
+    most_common = dictionary.most_common()
+
+    sorted_words = [word for word, _ in most_common]
 
 
     with open(f"data/empathic-dialogue-dictionary.txt", "w", encoding="utf-8") as f:
         for word in sorted_words:
             f.write(word + "\n")
 
-    # total items = 99646
-    print(f"total items={cntr}")
+
