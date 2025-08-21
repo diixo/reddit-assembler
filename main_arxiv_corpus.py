dfa9db8c54a0b80b728960c74f8272aa5d523948
diixo <anatolii.podoba@gmail.com>
2025-08-21 20:35:07 +0300

    main_arxiv_corpus.py: len(w) > 3

--- main_arxiv_corpus.py
+++ main_arxiv_corpus.py
@@ -117,7 +117,7 @@ def processing(dictionary: Counter):
 
             tokens = str_tokenize_words(txt)
 
-            dictionary.update([ w for w in tokens if len(w) > 1 ])
+            dictionary.update([ w for w in tokens if len(w) > 3 ])
 
     print(f"total items={idx}")
 
