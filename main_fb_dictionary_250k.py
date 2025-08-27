a4281c271f8e04928d4dce924cd3f2cb306c5032
diixo <anatolii.podoba@gmail.com>
2025-08-27 18:34:45 +0300

    main_fb_dictionary_250k.py: updated

--- main_fb_dictionary_250k.py
+++ main_fb_dictionary_250k.py
@@ -18,8 +18,18 @@ def main():
     word_dict = dictionary["words"]
     wordcounts = dictionary["wordcounts"].tolist()[:250_000]
 
+    pt_dictionary = {
+        "iwords": word_list,
+        "words": { w: word_dict[w] for i, w in enumerate(word_list) }
+    }
+    torch.save(pt_dictionary, f"data/{filename}.pth")
+
+    #################################################################
+
     assert len(word_list) == len(wordcounts), str(len(wordcounts))
 
+    with Path(f"data/{filename}.json").open("w", encoding="utf-8") as f:
+        json.dump(dict(zip(word_list, wordcounts)), f, indent=2)
 
     path = Path(f"data/{filename}.txt")
     with path.open("w", encoding="utf-8") as f:
@@ -27,16 +37,6 @@ def main():
             f.write(word + "\n")
         f.flush()
 
-
-    with Path(f"data/{filename}.json").open("w", encoding="utf-8") as f:
-        json.dump(dict(zip(word_list, wordcounts)), f, indent=2)
-
-
-    pt_dictionary = {
-        "iwords": word_list,
-        "words": { w: word_dict[w] for i, w in enumerate(word_list) }
-    }
-    torch.save(pt_dictionary, f"data/{filename}.pth")
 
 
 if __name__ == "__main__":
