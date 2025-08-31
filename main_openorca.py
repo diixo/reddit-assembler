91bd12b87161079ae29d514bf1d6eb70be6bb2e3
diixo <anatolii.podoba@gmail.com>
2025-08-31 15:31:51 +0300

    main_openorca.py: updated

--- main_openorca.py
+++ main_openorca.py
@@ -5,7 +5,28 @@ from datasets import load_dataset, Dataset
 from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
 from torch.utils.data import DataLoader
 
+from transformers import GPT2Tokenizer
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+
 
 dataset = load_dataset("Open-Orca/OpenOrca", split="train", streaming=False)
 
 print(f"Size: {len(dataset)}")
+
+
+def count_tokens(example):
+    return {
+        "n_tokens": sum(len(tokenizer.encode(example[field])) for field in ["system_prompt","question","response"])
+    }
+
+
+#
+dataset = dataset.map(count_tokens, batched=False)
+
+#
+total_tokens = sum(dataset["n_tokens"])
+print(f"Total tokens in DS: {total_tokens}")
+
+# 1_592_792_390
