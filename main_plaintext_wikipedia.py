26939ed562177ea69a581ad5224c4f6e1c28bfbc
diixo <anatolii.podoba@gmail.com>
2025-08-31 22:37:49 +0300

    main_plaintext_wikipedia.py: updated

--- main_plaintext_wikipedia.py
+++ main_plaintext_wikipedia.py
@@ -7,17 +7,7 @@ from utils import str_tokenize_words
 
 input_dir = './datasets/wikipedia-fullEnglish'
 output_dir = './train-results'
-
-
-def read_from_file(filename):
-    f = open(filename, "r", encoding="utf-8")
-    words = 0
-    lines = f.readlines()
-    for line in lines:
-        tokens = str_tokenize_words(line)
-        words += len(tokens)
-
-    return len(lines), words
+output_file = "plaintext-wikipedia-en.txt"
 
 
 if __name__ == "__main__":
@@ -30,34 +20,37 @@ if __name__ == "__main__":
 
     sents = 0
     words = 0
+    max_words = 0
 
-    for root, _, filenames in walk_list:
-        if filenames:
-            relative_dir = os.path.relpath(root, input_dir)
-            output_file = os.path.join(output_dir, f"{relative_dir.replace(os.sep, '_')}.txt")
-            intermediate_files.append(output_file)
+    os.makedirs(output_dir, exist_ok=True)
 
-            for filename in sorted(filenames):
-                file_path = os.path.join(root, filename)
-                count_s, count_w = read_from_file(file_path)
+    with open(os.path.join(output_dir, output_file), 'w', encoding="utf-8") as outfile:
 
-                sents += count_s
-                words += count_w
+        for root, _, filenames in walk_list:
+            if filenames:
+                relative_dir = os.path.relpath(root, input_dir)
+                output_file = os.path.join(output_dir, f"{relative_dir.replace(os.sep, '_')}.txt")
+                intermediate_files.append(output_file)
 
-            print(f"Read files({len(filenames)}) in {root} [...sents={sents} ...words={words}]")
+                for filename in sorted(filenames):
+                    file_path = os.path.join(root, filename)
 
-            #os.makedirs(os.path.dirname(output_file), exist_ok=True)
+                    f = open(file_path, "r", encoding="utf-8")
+                    lines = f.readlines()
+                    for line in lines:
+                        tokens = str_tokenize_words(line)
+                        words += len(tokens)
+                        max_words = max(max_words, len(tokens))
 
-            # with open(output_file, 'w') as outfile:
-            #     for filename in sorted(filenames):
-            #         file_path = os.path.join(root, filename)
-            #         try:
-            #             with open(file_path, 'r') as infile:
-            #                 outfile.write(infile.read())
-            #                 outfile.write("\n")
-            #         except Exception as e:
-            #             print(f"Error reading {file_path}: {e}")
-            #print(f"Concatenated files in {root} into {output_file}")
+                        outfile.write(line.strip())
+                        outfile.write("\n")
 
-    print("sents:", sents, "words:", words) # sents: 79_087_472 words: 1_953_810_569
+                    sents += len(lines)
 
+                print(f"Read files({len(filenames)}) in {root} [...sents={sents} ...words={words}]")
+    ################################################################################################
+    print(
+        "sents:", sents,
+        "words:", words,
+        "max_words_sent:", max_words,
+        "avg_words_sent:", int(words/sents) + 1) # sents: 79_087_472 words: 1_953_810_569
